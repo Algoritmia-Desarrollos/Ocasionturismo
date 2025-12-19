60469b3eb84a8b56731cc04b765e302e89e7c00b
lucagazze <lucagazze1@gmail.com>
2025-12-19 16:26:34 -0300

    2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}"], // Esto le dice: "buscá clases en todos los html y js"
   theme: {
     extend: {
       colors: {
-        primary: "#FBBF24", // Tu amarillo
-        secondary: "#1F2937", // Tu gris oscuro
+        primary: "#FBBF24",
+        secondary: "#1F2937",
         "background-light": "#FFFFFF",
         "text-main": "#1F2937",
         "text-muted": "#4B5563",
       },
       fontFamily: {
-        // Aquí configuramos la fuente que pediste en el HTML
-        display: ['"Anton"', "sans-serif"],
-        body: ['"Montserrat"', "sans-serif"],
-      },
-      screens: {
-        '3xl': '1920px',
+        display: ["Anton", "sans-serif"],
+        body: ["Montserrat", "sans-serif"],
       },
     },
   },
